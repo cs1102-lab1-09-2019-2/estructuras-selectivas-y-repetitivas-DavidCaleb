103a73f4a0e37765b2fd220804b6f0053d297bab
DavidCaleb <54277172+DavidCaleb@users.noreply.github.com>
2019-09-01 20:45:11 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
 
+using namespace std; 
 
 int main() {
+  int num,max,i,min,n;
+  
+  cout<<"Ingrese cuantos numeros ingresara: ";
+  cin>>n;
+  
+  for(i=0;i<n;i++){
+    
+    cout<<"Ingrese el numero: ";
+    cin>>num;
 
-    return 0;
+    if (i==0){
+      max = num;
+      
+    }
+    else{
+      if(num>max)max=num;
+      
+    }
+  }
+  cout<<"el numero mayor es: "<<max;
+return 0;
 }

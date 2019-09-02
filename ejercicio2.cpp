6eb7dad79ba757b4188b07af6f0694570dc3f799
DavidCaleb <54277172+DavidCaleb@users.noreply.github.com>
2019-09-01 20:24:26 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
 
+using namespace std; 
 
 int main() {
+  
+int num,i,j;
 
-    return 0;
+
+  cin>>num;
+  
+  for (i=0;i<num;i++){
+    
+    for (j=0;j<num;j++){
+      if(j==i)
+      {cout<<"1";
+      }else if(i==0 && j>=1){
+        cout<<"1";
+      }else if(i==num-1 && j<=num-1){
+        cout<<"1";
+      }else if(i>0 && j==0){
+        cout<<"1";
+      }else if(i<num-1 && j==num-1){
+        cout<<"1";
+      }else {
+        cout<<" ";
+      }
+    }
+    cout<<"\n";
+  }
+  return 0;
 }
